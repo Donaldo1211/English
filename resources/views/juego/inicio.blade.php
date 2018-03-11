914b7b4340ce8eb719fe2357efa042a059bd5e8b
Donaldo Lopez <LUIS12_94@hotmail.com>
2018-03-10 23:45:27 -0600

    Cambiar border color de input respecto al length.

--- resources/views/juego/inicio.blade.php
+++ resources/views/juego/inicio.blade.php
@@ -1,35 +1,40 @@
 @extends('plantilla.main')
-<link rel="stylesheet" href="css/juego.css" />
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-<script src="http://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js"></script>
-<script src="plugins/js/stepper.widget.js"></script>
+
+
 @section('content')
-  <style>
-  .stepper-widget { margin:30px auto;}
-  </style>
-    <div class="container">
-        <div class="row">
-            <div class="form group">
-              <h2>Numero de verbos:</h2>
-              <form class="form-inline">
-                    <div class="stepper-widget">
-                        <input type="text" class="js-qty-input form-control" value="1" />
-                        <button type="button" class="js-qty-down btn btn-primary">-</button>
-                        <button type="button" class="js-qty-up btn btn-danger">+</button>
-                    </div>
-                    </form>
-            </div>
-            <div class="form-group col-md-12">
-              <button type="button" class="btn btn-success" id="iniciarJuego" name="button">Inicar</button>
-            </div>
+<div class="container">
+  <div class="col-md-12">
+    <div class="card">
+      <h5 class="card-header">Verb Test</h5>
+      <div class="card-body">
+        <h5 class="card-title">Selecciona el numero de verbos:</h5>
+        <p class="card-text"></p>
+        <div class="form-group">
+            {!!Form::open()!!}
+            {!!Form::label('Verbs', null, ['class' => 'control-label'])!!}
+            {!!Form::number('num', null,(['class' => 'form-control','min'=>0,'max'=>10,'id'=>'num']))!!}
+            {!! Form::close() !!}
         </div>
+        <a href="#" class="btn btn-primary">Jugar</a>
+      </div>
     </div>
-    <script>
-            jQuery.noConflict();
-        </script>
-    <script>
-          $(document).on('ready', function(){
-                   $('.stepper-widget').stepper();
-               });
-     </script>
+  </div>
+</div>
 @endsection
+@push('scripts')
+  <script type="text/javascript">
+  $(function() {
+     maxVal = 10;
+     $('#num').keyup(function(){
+        var insertedVal = $(this).val();
+        console.log(insertedVal);
+        if (insertedVal.length < maxVal){
+            $(this).css({"color":"red","border":"1px solid red"});
+        }else{
+            $(this).css({"color":"green","border":"1px solid green"});
+        }
+     })
+  });
+
+  </script>
+@endpush
--- resources/views/plantilla/main.blade.php
+++ resources/views/plantilla/main.blade.php
@@ -4,15 +4,11 @@
     <meta charset="utf-8">
     <title>Verbs</title>
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+  <link rel="stylesheet" href="{{asset('plugins/css/bootstrap.css')}}" />
 
-
-    <link rel="stylesheet" href="{{asset('plugins/css/bootstrap.css')}}" />
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/1.1.3/sweetalert.min.css" />
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/1.1.3/sweetalert.css">
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/1.1.3/sweetalert.min.js"></script>
+
 
 
   </head>
@@ -21,10 +17,17 @@
     <div class="row">
       <div class="container">
   @yield('content')
-      </div>
+    </div>
 
     </div>
     @include('plantilla.elementos.footer')
     @include('sweet::alert')
+
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/1.1.3/sweetalert.min.js"></script>
+    @stack('scripts')
   </body>
 </html>
